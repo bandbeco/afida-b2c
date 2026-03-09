8faee57fda3ecf6211d6eaa398ec811d229ab3dc
Laurent Curau <github@lqro.slmail.me>
2026-03-09 12:33:20 +0100

    Remove Vegware collection card from Shop by Category grid

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,7 +7,6 @@ class PagesController < ApplicationController
                                 .limit(8)
     @featured_straw_product = Product.catalog_products.find_by(slug: "bio-fibre-straws")
     @categories = Category.browsable.subcategories.includes(:parent).with_attached_image
-    @vegware_collection = Collection.find_by(slug: Collection::VEGWARE_SLUG)
     @collections = Collection.regular.featured.where.not(slug: Collection::VEGWARE_SLUG).by_position.with_attached_image
     @client_logos = client_logos
   end
