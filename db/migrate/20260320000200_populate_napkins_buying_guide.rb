ac31e6e93915523eccd5c9ea24f01975d04bb834
Laurent Curau <github@lqro.slmail.me>
2026-03-20 14:19:38 +0100

    Add buying guide for Napkins category

---
+++ db/migrate/20260320000200_populate_napkins_buying_guide.rb
@@ -0,0 +1,10 @@
+class PopulateNapkinsBuyingGuide < ActiveRecord::Migration[8.1]
+  def up
+    buying_guide = File.read(Rails.root.join("lib/data/buying-guides/napkins.md"))
+    Category.find_by(slug: "napkins")&.update!(buying_guide: buying_guide)
+  end
+
+  def down
+    Category.find_by(slug: "napkins")&.update!(buying_guide: nil)
+  end
+end
