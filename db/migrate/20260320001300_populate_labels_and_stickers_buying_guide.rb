becf298e2a5d977a707f9fad1f04f8c86e583bc8
Laurent Curau <github@lqro.slmail.me>
2026-03-20 14:19:52 +0100

    Add buying guide for Labels & Stickers category

---
+++ db/migrate/20260320001300_populate_labels_and_stickers_buying_guide.rb
@@ -0,0 +1,10 @@
+class PopulateLabelsAndStickersBuyingGuide < ActiveRecord::Migration[8.1]
+  def up
+    buying_guide = File.read(Rails.root.join("lib/data/buying-guides/labels-and-stickers.md"))
+    Category.find_by(slug: "labels-and-stickers")&.update!(buying_guide: buying_guide)
+  end
+
+  def down
+    Category.find_by(slug: "labels-and-stickers")&.update!(buying_guide: nil)
+  end
+end
