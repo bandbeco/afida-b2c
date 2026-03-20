6e91b4411f933e6ddf32b0fff4967c035a9f3c85
Laurent Curau <github@lqro.slmail.me>
2026-03-20 14:19:41 +0100

    Add buying guide for Cold Cups category

---
+++ db/migrate/20260320000400_populate_cold_cups_buying_guide.rb
@@ -0,0 +1,10 @@
+class PopulateColdCupsBuyingGuide < ActiveRecord::Migration[8.1]
+  def up
+    buying_guide = File.read(Rails.root.join("lib/data/buying-guides/cold-cups.md"))
+    Category.find_by(slug: "cold-cups")&.update!(buying_guide: buying_guide)
+  end
+
+  def down
+    Category.find_by(slug: "cold-cups")&.update!(buying_guide: nil)
+  end
+end
